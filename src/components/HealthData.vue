fbea543f51c88254dd6d088790a36841939d6c8f
Geoseanlee <lee.zixi.george@gmail.com>
2024-10-16 17:25:03 +1100

    Link between UpdaInfo and HealthData

--- src/components/HealthData.vue
+++ src/components/HealthData.vue
@@ -3,8 +3,10 @@
       <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect"
           :background-color="backgroundColor" :text-color="textColor" :active-text-color="activeTextColor">
           <el-sub-menu index="1">
-              <template v-slot:title><img src="../assets/logo.png" alt="Image 1" style="width: 20px; height: 20px; margin-right: 8px;"></template>
-              <el-menu-item index="1-1">Manage Personal Information</el-menu-item>
+              <template v-slot:title>
+                <img src="../assets/logo.png" alt="Image 1" style="width: 20px; height: 20px; margin-right: 8px; cursor: pointer;" @click="goToPage('/UpdateInformation')">
+              </template>
+              <el-menu-item index="1-1" @click="goToPage('/UpdateInformation')">Manage Personal Information</el-menu-item>
               <el-menu-item index="1-2" @click="goToPage('/HealthData')">Health Data</el-menu-item>
               <el-menu-item index="1-3" @click="logout">Log out</el-menu-item>
           </el-sub-menu>
